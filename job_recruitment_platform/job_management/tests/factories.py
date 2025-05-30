2c35b02fe23b5705831bed5b2dd3379866a96b37
Ashu <ashutoshkhade2530@gmail.com>
2025-05-30 19:25:20 +0530

    Define factories.py for generating test data models

--- job_recruitment_platform/job_management/tests/factories.py
+++ job_recruitment_platform/job_management/tests/factories.py
@@ -5,7 +5,6 @@ from django.contrib.auth.hashers import make_password
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
-
     
     email = factory.Sequence(lambda n: "person{}@example.com".format(n))
     password = make_password("TestPass")
