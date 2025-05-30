fca2dbee24614507bc1cfe28874dcb4903c2057e
Ashu <ashutoshkhade2530@gmail.com>
2025-05-30 19:31:05 +0530

    Define models.py with user profile and job management data structures

--- job_recruitment_platform/job_management/models.py
+++ job_recruitment_platform/job_management/models.py
@@ -39,7 +39,6 @@ class PendingUser(BaseModel):
             return False
         return True
 
-
 class Token(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
