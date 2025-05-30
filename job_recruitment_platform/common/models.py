c3dd512fd61597ba45944ff377152a0cce46cb54
Ashu <ashutoshkhade2530@gmail.com>
2025-05-30 18:45:30 +0530

    Integrate core data models in common/models.py

--- job_recruitment_platform/common/models.py
+++ job_recruitment_platform/common/models.py
@@ -1,7 +1,6 @@
 
 from django.db import models
 import uuid
-
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
