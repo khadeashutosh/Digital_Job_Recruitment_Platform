a513baece8a68e649b854c1c0f677be063208193
Ashu <ashutoshkhade2530@gmail.com>
2025-06-04 14:02:52 +0530

    Map root URL patterns for project-wide routing

--- job_recruitment_platform/job_recruitment_platform/urls.py
+++ job_recruitment_platform/job_recruitment_platform/urls.py
@@ -22,7 +22,7 @@ from application_tracking.views import list_adverts
 urlpatterns = [
     path("", list_adverts, name="home"),
     path('admin/', admin.site.urls),
-    path("auth/", include("accounts.urls")),
+    path("auth/", include("job_management.urls")),
     path("adverts/", include("application_tracking.urls")),
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
