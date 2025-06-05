0019b4e9e1b2103723d413cf2e3b525a6caf4247
Ashu <ashutoshkhade2530@gmail.com>
2025-06-05 07:46:59 +0530

    Set up celery intergration for asyncronous tasks handling

--- job_recruitment_platform/job_recruitment_platform/celery.py
+++ job_recruitment_platform/job_recruitment_platform/celery.py
@@ -2,9 +2,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talent_base.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job_recruitment_platform.settings')
 
-app = Celery('talent_base')
+app = Celery('job_recruitment_platform')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
